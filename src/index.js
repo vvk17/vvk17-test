99fc086107f9a097b017d411e554953d04310ab2
Valery Kornienko <v12345@mail.ru>
2017-02-01 18:05:22 +0300

    second

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.css';
+import numeral from 'numeral';
+
+const courseValue = numeral(300).format('$0,0.00');
+debugger;
+console.log(`I would not pay even ${courseValue} for this interesting course`);
