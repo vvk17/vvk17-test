da31f424ac246e4d2bd3974218a6d44778524817
Valery Kornienko <v12345@mail.ru>
2017-02-10 18:48:59 +0300

    semi-final commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 import './index.css';
 import numeral from 'numeral';
+import {getUsers, deleteUser} from './api/userApi';
 
 const courseValue = numeral(300).format('$0,0.00');
-debugger;    //eslint-disable-line no-debugger
+// debugger;    //eslint-disable-line no-debugger
 console.log(`I would not pay even ${courseValue} for this interesting course`); //eslint-disable-line no-console
+
+// Populate table of users via API call
+getUsers().then ( result => {
+  let usersBody = "";
+
+  result.forEach (user => {
+    usersBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+    </tr>`;
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  Array.from (deleteLinks, link => {
+    link.onclick = function (event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser (element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+});
